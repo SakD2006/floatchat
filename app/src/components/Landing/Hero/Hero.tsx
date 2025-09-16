ef15e72003362caa11bc68aad074a6953d5288c9
Upayan M <upayanm3@gmail.com>
2025-09-17 03:20:03 +0530

    feat: Add new components and icons for Landing page
    - Introduced new SVG icons: carbon_quadrant-plot, gravity-ui_thunderbolt, lucide_brick-wall, material-symbols_settings-rounded.
    - Created KeyFeatures component to showcase key features with icons and descriptions.
    - Implemented Hero component for the landing page with a heading and description.
    - Added WhatsTheProblem and WhatDoesFloatChatDo components to explain the problem and solution.
    - Integrated WhyItMatters component to highlight the importance of the project.
    - Updated layout to include a Footer component.
    - Refactored Home component to use Landing instead of Hero.
    - Added Line and SubHeading components for better UI structure.
    - Adjusted Profile component styles for improved layout.

--- app/src/components/Landing/Hero/Hero.tsx
+++ app/src/components/Landing/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import { Heading } from "@/components/ui";
 
 export default function Home() {
   return (
-    <div className="select-none">
+    <div className="select-none min-h-screen flex flex-col justify-center items-center px-4">
       <Heading text="FloatChat" />
       <p className="text-center text-sm sm:text-xl mb-4 text-gray-300">
         AI Powered Conversational Interface for ARGO Data Discovery and
--- app/src/components/ui/Footer/Footer.tsx
+++ app/src/components/ui/Footer/Footer.tsx
@@ -1,7 +1,13 @@
 export default function Footer() {
   return (
     <div className="w-full bg-[#1FF4FF] border-t border-gray-300 text-end p-4 text-sm text-gray-600 z-100">
-      Developed by BoyOhBuoy | SIH 25
+      <a
+        href="https://github.com/SakD2006/floatchat"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Developed by BoyOhBuoy | SIH 25
+      </a>
     </div>
   );
 }
